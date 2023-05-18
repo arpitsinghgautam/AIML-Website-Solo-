a07a079699f2e2eea98394ca0f0da22acd5bcb3c
Arpit Singh Gautam <arpitsinghgautam777@gmail.com>
2023-05-18 14:22:58 +0530

    Updated the Website

--- assets/js/main.js
+++ assets/js/main.js
@@ -313,6 +313,30 @@
          ===============================================*/
         $('.advisor-carousel').owlCarousel({
             loop: true,
+            nav: true,
+            margin:10,
+            dots: true,
+            autoplay: true,
+            items: 1,
+            navText: [
+                "<i class='fa fa-angle-left'></i>",
+                "<i class='fa fa-angle-right'></i>"
+            ],
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 2
+                },
+                1000: {
+                    items: 3,
+                }
+            }
+        });
+
+        $('.advisor-carousel-no-nav').owlCarousel({
+            loop: true,
             nav: false,
             margin:30,
             dots: true,
